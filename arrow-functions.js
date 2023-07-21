572b26af321066b6d09e8960ae6e3f5958e2f27b
Cristian Hanganu <cristihell23@yahoo.com>
2023-07-21 18:18:42 +0000

    Different ways to writte Arrow-Functions.

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,32 @@
  */
 
 // Vanilla JavaScript Function
+// NON - ES6
+function addTwoNumbers1(a, b){
+    return a+b;
+}
 
+let sum1 = addTwoNumbers1(5,7);
+console.log(sum);
 // Arrow Function With Parameters
-
+const addTwoNumbers = (a,b) => {
+    return a+b;
+}
+let sum2 = addTwoNumbers(7,2);
+console.log(sum);
 // Single Line Arrow Function With Parameters
-
+// const addTwoNumbers2 = (a,b) => (a+b); - VALID
+const addTwoNumbers2 = (a,b) => a+b;
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('This is a sentance');
 
+const sayHello = () => console.log('Hello');
+sayHello();
 // Returning Multiple Lines
+const returnMultipleLines = () => {
+    `<p>
+        This is a multiline string !
+    </p>`
+}
+console.log(returnMultipleLines());
